af5b674f7dc6bd87775d4448b24af3344245b009
gmanninglive <gmanning@live.co.uk>
2021-10-07 12:02:28 +0100

    Combine s3 Upload function to test deployment err

--- pages/api/business/gets3url.js
+++ pages/api/business/gets3url.js
@@ -1,4 +1,17 @@
-import { generateUploadURL } from "middleware/s3";
+import { S3 }  from 'aws-sdk'
+import { v4 as uuidv4 } from 'uuid';
+
+const bucket_name = process.env.NEXT_PUBLIC_BUCKET_NAME;
+const region = process.env.NEXT_PUBLIC_BUCKET_REGION;
+const access_key = process.env.NEXT_PUBLIC_BUCKET_ACCESS_KEY;
+const secret_key = process.env.NEXT_PUBLIC_BUCKET_SECRET_KEY;
+
+const bucket = new S3({
+    region,
+    access_key,
+    secret_key,
+    signatureVersion: 'v4'
+})
 
 export default async function handler(req, res) {
 if(req.method === "POST"){
@@ -14,4 +27,17 @@ if(req.method === "POST"){
          
         })
     }
+}
+
+
+export async function generateUploadURL(ext){
+    const imageName = uuidv4();;
+
+    const params = ({
+        Bucket: bucket_name,
+        Key: `${imageName.toString()}${ext}`,
+        Expires: 60
+    })
+    const uploadURL = await bucket.getSignedUrlPromise('putObject', params)
+    return uploadURL;
 }
